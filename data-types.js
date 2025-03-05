c0f3cc64b13c9b61c3bcb92de7608fd2a5e6318a
Erick Mongare <erickmongaremose@gmail.com>
2025-03-05 12:17:22 +0300

    feat: add data types code

---
+++ data-types.js
@@ -0,0 +1,37 @@
+const classRep1 = 'Alfa Mmosi';
+const classRep2 = 'Mercy Chepkemoi';
+const classTitle = 'Invictus';
+const classClown = 'Eugene Wekesa';
+
+// define age variables for the class reps
+
+const classRep1Age = 39;
+const classRep2Age = 18;
+
+const isClassRep1Rich = true;
+const isClassRep2Rich = true;
+
+// comic relief
+const isEugeneRich = false;
+
+console.log(classRep1 + ' : ' + classRep1Age);
+console.log(classRep2 + ' : ' + classRep2Age);
+
+// log booleans
+
+console.log('is ' + classClown + ' Rich ?' + ' : ' + isEugeneRich);
+console.log('is ' + classRep1 + ' Rich ?' + ' : ' + isClassRep1Rich);
+console.log('is ' + classRep2 + ' Rich ?' + ' : ' + isClassRep2Rich);
+
+// string interpolation
+// double quotes
+// single quotes
+// backticks
+
+const sentence = `Was ${classClown} Rich in the past? ${isEugeneRich}, but he will be rich in the future, possibly`;
+
+console.log(sentence);
+
+// the rules of creating variables
+// camelCase
+// snake_case
