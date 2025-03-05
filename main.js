e6942c7abbcc1085413dfbbb6d01aefa8723efa2
Erick Mongare <erickmongaremose@gmail.com>
2025-03-05 12:16:45 +0300

    feat: add variables code

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
+// variables
+const classRep1 = 'Alfa Mmosi';
+const classRep2 = 'Mercy Chepkemoi';
+const classTitle = 'Invictus';
 
+// change the class rep
+// classRep1 = 'Brian Kaloki';
+
+// retrieve the data
+// log it into the console
+console.log(classTitle);
+console.log(classRep2);
+console.log(classRep1);
+
+// variable declaration in JS
+// const, let and var
+// keywords
+// for, const, let, function, var, if
+
+let technicalMentor;
+const timeKeeper = 'Arnold Kulavi';
+
+technicalMentor = 'Edwin Kipyego';
+technicalMentor = 'Eugene Wekesa';
+
+console.log(technicalMentor);
+console.log(timeKeeper);
+
+// quiz
+
+let username = 'celestyn-dev';
+
+// changing or reassigning the variable
+username = 'badgirlriri';
+console.log(username);
